6a1d13fe306c299cc0fac62fac54cd06864204a2
sarabrzostek <sarabtzostek@wp.pl>
2020-07-07 20:41:18 +0200

    Updates

---
+++ precache-manifest.e86636fc57d50a51cf555371d22c74ca.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31e74a7642fc3bfa9dc32da88d8bb00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "398a3c524e0f9d8cad77",
+    "url": "/portfolio/static/css/main.f32587af.chunk.css"
+  },
+  {
+    "revision": "3e8ac7eb35f23fd4e2c3",
+    "url": "/portfolio/static/js/2.5675bff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5675bff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398a3c524e0f9d8cad77",
+    "url": "/portfolio/static/js/main.aa93a86c.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "200b30576685ccd8ba5d78b632819bbf",
+    "url": "/portfolio/static/media/all.200b3057.zip"
+  },
+  {
+    "revision": "bf75476f8433121de6ea945658f0b6b9",
+    "url": "/portfolio/static/media/anim.bf75476f.svg"
+  },
+  {
+    "revision": "760b8b6a38a58aab51a584596197f340",
+    "url": "/portfolio/static/media/app-web.760b8b6a.zip"
+  },
+  {
+    "revision": "06fd0dd1bf9b70540c9bde5599d67ec4",
+    "url": "/portfolio/static/media/app.06fd0dd1.jpg"
+  },
+  {
+    "revision": "b9e41ff48711ac6bc76a1da9e03af888",
+    "url": "/portfolio/static/media/bg-contact-mobile.b9e41ff4.svg"
+  },
+  {
+    "revision": "fde059e2882c35e640f7dce98339ac4c",
+    "url": "/portfolio/static/media/bg-home-mobile.fde059e2.svg"
+  },
+  {
+    "revision": "ab9b21a07ddf082370c265f2607d5d76",
+    "url": "/portfolio/static/media/bg-skills-mobile.ab9b21a0.svg"
+  },
+  {
+    "revision": "563275c868bde9fa29d40841f7c069c0",
+    "url": "/portfolio/static/media/contact_bg.563275c8.svg"
+  },
+  {
+    "revision": "b1b10588b9a4175ae5da348a0d93f917",
+    "url": "/portfolio/static/media/css.b1b10588.svg"
+  },
+  {
+    "revision": "ed89e8e8956f52a370c97aad1ef8e30c",
+    "url": "/portfolio/static/media/dimension.ed89e8e8.svg"
+  },
+  {
+    "revision": "0d88aa02dec360d0f2c3bcc7c6a8b401",
+    "url": "/portfolio/static/media/html.0d88aa02.svg"
+  },
+  {
+    "revision": "7ccace6049f19ea04b4f3421ddb18b92",
+    "url": "/portfolio/static/media/il.7ccace60.svg"
+  },
+  {
+    "revision": "b5c548ba6cefc65bafaa514a8fcc4358",
+    "url": "/portfolio/static/media/indesign.b5c548ba.svg"
+  },
+  {
+    "revision": "be0237a46feb2f9d9d03e1c43a937978",
+    "url": "/portfolio/static/media/landing-video.be0237a4.mp4"
+  },
+  {
+    "revision": "0eae7e37e842d96cc5b970fba20facfb",
+    "url": "/portfolio/static/media/landing_page_bg.0eae7e37.svg"
+  },
+  {
+    "revision": "d266d7c752cad3d1985e4ae18487afa5",
+    "url": "/portfolio/static/media/other.d266d7c7.zip"
+  },
+  {
+    "revision": "edcbf9b34985e26230ff550e23bc4f67",
+    "url": "/portfolio/static/media/other.edcbf9b3.jpg"
+  },
+  {
+    "revision": "d111f6c13cfb3030a79ef18b7fdb36a2",
+    "url": "/portfolio/static/media/pp.d111f6c1.svg"
+  },
+  {
+    "revision": "e8a671a14a66642ed4ce916a7bb30662",
+    "url": "/portfolio/static/media/ps.e8a671a1.svg"
+  },
+  {
+    "revision": "e8036a64265ffe1a79f8a7fb54ebfd10",
+    "url": "/portfolio/static/media/skills_bg.e8036a64.svg"
+  },
+  {
+    "revision": "9478ef6ee33e9e5ce0caf1edb5de17f7",
+    "url": "/portfolio/static/media/vc.9478ef6e.jpg"
+  },
+  {
+    "revision": "e692b2cabe7f982802556fd52749b827",
+    "url": "/portfolio/static/media/vc.e692b2ca.zip"
+  },
+  {
+    "revision": "a4294037aa116a4ec6d655ca4182c162",
+    "url": "/portfolio/static/media/wordpress.a4294037.svg"
+  },
+  {
+    "revision": "283027b80a4ed1441535b1922aac988c",
+    "url": "/portfolio/static/media/xd.283027b8.svg"
+  },
+  {
+    "revision": "d19d309c85568f217fff543eef7001ec",
+    "url": "/portfolio/static/media/zarzeczny_maciej_cv_eng.d19d309c.pdf"
+  }
+]);
